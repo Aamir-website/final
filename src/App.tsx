9bec0287745ebff7a078989223a52e0fda78af5f
Aamir-website <doozboi99@gmail.com>
2025-08-20 11:12:47 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -176,7 +176,6 @@ function App() {
           trigger: heroRef.current,
           start: "bottom bottom",
           end: "bottom+=-50 top",
-          scrub: 1,
           ease: "power2.out",
         }
       });
