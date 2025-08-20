1883feb963210027cc7390260f7ae21ee4adf116
Aamir-website <doozboi99@gmail.com>
2025-08-20 11:11:46 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,7 @@ function App() {
     // Background text animation
     if (backgroundTextRef.current) {
       gsap.to(backgroundTextRef.current, {
-        y: -400,
+        y: -200,
         opacity: 1,
         scaleY: 1,
         scrollTrigger: {
